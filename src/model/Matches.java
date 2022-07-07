99197bc079084f7feaca2123ee404cc828e485e2
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2022-07-06 21:25:31 -0300

    Update Matches

---
+++ src/model/Matches.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Matches {
+
+	Team homeTeam;
+	Team awayTeam;
+	int homeScore;
+	int awayScore;
+	int homeScoreHT;
+	int awayScoreHT;
+
+	public Team getHomeTeam() {
+		return homeTeam;
+	}
+
+	public void setHomeTeam(Team homeTeam) {
+		this.homeTeam = homeTeam;
+	}
+
+	public Team getAwayTeam() {
+		return awayTeam;
+	}
+
+	public void setAwayTeam(Team awayTeam) {
+		this.awayTeam = awayTeam;
+	}
+
+	public int getHomeScore() {
+		return homeScore;
+	}
+
+	public void setHomeScore(int homeScore) {
+		this.homeScore = homeScore;
+	}
+
+	public int getAwayScore() {
+		return awayScore;
+	}
+
+	public void setAwayScore(int awayScore) {
+		this.awayScore = awayScore;
+	}
+
+	public int getHomeScoreHT() {
+		return homeScoreHT;
+	}
+
+	public void setHomeScoreHT(int homeScoreHT) {
+		this.homeScoreHT = homeScoreHT;
+	}
+
+	public int getAwayScoreHT() {
+		return awayScoreHT;
+	}
+
+	public void setAwayScoreHT(int awayScoreHT) {
+		this.awayScoreHT = awayScoreHT;
+	}
+
+}
