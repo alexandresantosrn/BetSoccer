1cd67073c9aaa1ff30e4689f220915054c5877ab
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2022-07-06 21:20:57 -0300

    Update Teams

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,26 +1,51 @@
 package model;
 
 public class Team {
-	
+
 	String name;
-	String country;
-	
+	int matches;
+	int matchGoals;
+	int matchGoalsFor;
+	int matchGoalsAverage;
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getCountry() {
-		return country;
+
+	public int getMatches() {
+		return matches;
 	}
-	
-	public void setCountry(String country) {
-		this.country = country;
+
+	public void setMatches(int matches) {
+		this.matches = matches;
 	}
-	
-	
+
+	public int getMatchGoals() {
+		return matchGoals;
+	}
+
+	public void setMatchGoals(int matchGoals) {
+		this.matchGoals = matchGoals;
+	}
+
+	public int getMatchGoalsFor() {
+		return matchGoalsFor;
+	}
+
+	public void setMatchGoalsFor(int matchGoalsFor) {
+		this.matchGoalsFor = matchGoalsFor;
+	}
+
+	public int getMatchGoalsAverage() {
+		return matchGoalsAverage;
+	}
+
+	public void setMatchGoalsAverage(int matchGoalsAverage) {
+		this.matchGoalsAverage = matchGoalsAverage;
+	}
 
 }
