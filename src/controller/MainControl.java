d750b065ba3aa86dfafc11f51c5d91c257fb0237
Alexandre Dantas dos Santos <allexsantosrn@gmail.com>
2022-07-30 23:19:54 -0300

    Updating matches

--- src/controller/MainControl.java
+++ src/controller/MainControl.java
@@ -120,77 +120,91 @@ public class MainControl {
 
 		Team team1 = repository.searchTeam(home);
 		Team team2 = repository.searchTeam(away);
-		
-		//Team 1 Matches
-		team1.setMatches(team1.getMatches()+1);
-		team1.setMatchesHome(team1.getMatchesHome()+1);
-		
-		//Team 1 Goals
-		team1.setTotalgoals(team1.getTotalgoals()+Integer.parseInt(gfHome)+Integer.parseInt(gfAway));	
-		team1.setTotalgoalsFor(team1.getTotalgoalsFor()+Integer.parseInt(gfHome));
-		team1.setTotalgoalsAverage(team1.getTotalgoalsAverage()+Integer.parseInt(gfAway));
-		team1.setTotalgoalsForHome(team1.getTotalgoalsForHome()+Integer.parseInt(gfHome));
-		team1.setTotalgoalsAverageHome(team1.getTotalgoalsAverageHome()+Integer.parseInt(gfAway));
-		
-		//Team 1 Goals Avg
-		team1.setAvgGoals(team1.getTotalgoals()/team1.getMatches());
-		team1.setAvgGoalsFor(team1.getTotalgoalsFor()/team1.getMatches());
-		team1.setAvgGoalsAverage(team1.getTotalgoalsAverage()/team1.getMatches());
-		team1.setAvgGoalsForHome(team1.getAvgGoalsForHome()/team1.getMatchesHome());
-		team1.setAvgGoalsAverageHome(team1.getAvgGoalsAverageHome()/team1.getMatchesHome());
-		
-		//Team 1 Overs		
-		if((Integer.parseInt(gfHome)+Integer.parseInt(gfAway)) > 1.5) {
-			
-			team1.setAvgOver15(team1.getAvgOver15()+1);
-			team1.setAvgOver15Home(team1.getAvgOver15Home()+1);			
+
+		// Team 1 Matches
+		team1.setMatches(team1.getMatches() + 1);
+		team1.setMatchesHome(team1.getMatchesHome() + 1);
+
+		// Team 1 Goals
+		team1.setTotalgoals(team1.getTotalgoals() + Integer.parseInt(gfHome) + Integer.parseInt(gfAway));
+		team1.setTotalgoalsFor(team1.getTotalgoalsFor() + Integer.parseInt(gfHome));
+		team1.setTotalgoalsAverage(team1.getTotalgoalsAverage() + Integer.parseInt(gfAway));
+		team1.setTotalgoalsForHome(team1.getTotalgoalsForHome() + Integer.parseInt(gfHome));
+		team1.setTotalgoalsAverageHome(team1.getTotalgoalsAverageHome() + Integer.parseInt(gfAway));
+
+		// Team 1 Goals Avg
+		team1.setAvgGoals(team1.getTotalgoals() / team1.getMatches());
+		team1.setAvgGoalsFor(team1.getTotalgoalsFor() / team1.getMatches());
+		team1.setAvgGoalsAverage(team1.getTotalgoalsAverage() / team1.getMatches());
+		team1.setAvgGoalsForHome(team1.getAvgGoalsForHome() / team1.getMatchesHome());
+		team1.setAvgGoalsAverageHome(team1.getAvgGoalsAverageHome() / team1.getMatchesHome());
+
+		// Team 1 Overs
+		if ((Integer.parseInt(gfHome) + Integer.parseInt(gfAway)) > 1.5) {
+
+			team1.setAvgOver15(team1.getAvgOver15() + 1);
+			team1.setAvgOver15Home(team1.getAvgOver15Home() + 1);
 		}
-		
-		if((Integer.parseInt(gfHome)+Integer.parseInt(gfAway)) > 2.5) {
-			
-			team1.setAvgOver25(team1.getAvgOver25()+1);
-			team1.setAvgOver25Home(team1.getAvgOver25Home()+1);			
+
+		if ((Integer.parseInt(gfHome) + Integer.parseInt(gfAway)) > 2.5) {
+
+			team1.setAvgOver25(team1.getAvgOver25() + 1);
+			team1.setAvgOver25Home(team1.getAvgOver25Home() + 1);
 		}
-		
-		//Team 2
-		team2.setMatches(team1.getMatches()+1);
-		team2.setMatchesAway(team2.getMatchesAway()+1);
-		
-		//Team 2 Goals
-		team2.setTotalgoals(team2.getTotalgoals()+Integer.parseInt(gfHome)+Integer.parseInt(gfAway));	
-		team2.setTotalgoalsFor(team2.getTotalgoalsFor()+Integer.parseInt(gfAway));
-		team2.setTotalgoalsAverage(team2.getTotalgoalsAverage()+Integer.parseInt(gfHome));
-		team2.setTotalgoalsForAway(team2.getTotalgoalsForAway()+Integer.parseInt(gfAway));
-		team2.setTotalgoalsAverageAway(team2.getTotalgoalsAverageAway()+Integer.parseInt(gfHome));
-		
-		//Team 2 Goals Avg
-		team2.setAvgGoals(team2.getTotalgoals()/team2.getMatches());
-		team2.setAvgGoalsFor(team2.getTotalgoalsFor()/team2.getMatches());
-		team2.setAvgGoalsAverage(team2.getTotalgoalsAverage()/team2.getMatches());
-		team2.setAvgGoalsForAway(team2.getAvgGoalsForAway()/team2.getMatchesAway());
-		team2.setAvgGoalsAverageAway(team2.getAvgGoalsAverageAway()/team2.getMatchesAway());
-		
-		//Team 2 Overs		
-		if((Integer.parseInt(gfHome)+Integer.parseInt(gfAway)) > 1.5) {
-			
-			team2.setAvgOver15(team2.getAvgOver15()+1);
-			team2.setAvgOver15Home(team2.getAvgOver15Home()+1);			
+
+		// Team 2
+		team2.setMatches(team1.getMatches() + 1);
+		team2.setMatchesAway(team2.getMatchesAway() + 1);
+
+		// Team 2 Goals
+		team2.setTotalgoals(team2.getTotalgoals() + Integer.parseInt(gfHome) + Integer.parseInt(gfAway));
+		team2.setTotalgoalsFor(team2.getTotalgoalsFor() + Integer.parseInt(gfAway));
+		team2.setTotalgoalsAverage(team2.getTotalgoalsAverage() + Integer.parseInt(gfHome));
+		team2.setTotalgoalsForAway(team2.getTotalgoalsForAway() + Integer.parseInt(gfAway));
+		team2.setTotalgoalsAverageAway(team2.getTotalgoalsAverageAway() + Integer.parseInt(gfHome));
+
+		// Team 2 Goals Avg
+		team2.setAvgGoals(team2.getTotalgoals() / team2.getMatches());
+		team2.setAvgGoalsFor(team2.getTotalgoalsFor() / team2.getMatches());
+		team2.setAvgGoalsAverage(team2.getTotalgoalsAverage() / team2.getMatches());
+		team2.setAvgGoalsForAway(team2.getAvgGoalsForAway() / team2.getMatchesAway());
+		team2.setAvgGoalsAverageAway(team2.getAvgGoalsAverageAway() / team2.getMatchesAway());
+
+		// Team 2 Overs
+		if ((Integer.parseInt(gfHome) + Integer.parseInt(gfAway)) > 1.5) {
+
+			team2.setAvgOver15(team2.getAvgOver15() + 1);
+			team2.setAvgOver15Home(team2.getAvgOver15Home() + 1);
 		}
-		
-		if((Integer.parseInt(gfHome)+Integer.parseInt(gfAway)) > 2.5) {
-			
-			team2.setAvgOver25(team2.getAvgOver25()+1);
-			team2.setAvgOver25Home(team2.getAvgOver25Home()+1);			
+
+		if ((Integer.parseInt(gfHome) + Integer.parseInt(gfAway)) > 2.5) {
+
+			team2.setAvgOver25(team2.getAvgOver25() + 1);
+			team2.setAvgOver25Home(team2.getAvgOver25Home() + 1);
 		}
+
+		// Matches
+		Match match = new Match();	
+
+		// Match
+		match.setHomeTeam(team1);
+		match.setAwayTeam(team2);
+		match.setHomeScore(Integer.parseInt(gfHome));
+		match.setAwayScore(Integer.parseInt(gfAway));
+
+		// Basic Team1
+		match.setAvgGoalsTotalHome(team1.getAvgGoals());
+		match.setAvgGoalsTotalForHome(team1.getAvgGoalsFor());
+		match.setAvgGoalsTotalAverageHome(team1.getAvgGoalsAverage());
+
+		// Basic Team2
+		match.setAvgGoalsTotalAway(team1.getAvgGoals());
+		match.setAvgGoalsTotalForAway(team1.getAvgGoalsFor());
+		match.setAvgGoalsTotalAverageAway(team1.getAvgGoalsAverage());
 		
-		//Matches		
-		Match match1 = new Match();
-		Match match2 = new Match();		
+		// Team1 Goals
+		match.setAvgGoalsHome(team1.getHome)
 		
-		match1.setHomeTeam(team1);
-		match1.setAwayTeam(team2);
-		match1.setHomeScore(Integer.parseInt(gfHome));
-		match1.setAwayScore(Integer.parseInt(gfAway));
 
 	}
 
